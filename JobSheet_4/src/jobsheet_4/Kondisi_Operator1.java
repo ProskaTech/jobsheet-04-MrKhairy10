13ad576795973d34dbad4ac15ca7b689e0820009
MrKhairy10 <112692947+MrKhairy10@users.noreply.github.com>
2022-09-15 08:29:13 +0700

    Add files via upload

---
+++ JobSheet_4/src/jobsheet_4/Kondisi_Operator1.java
@@ -0,0 +1,22 @@
+/*
+    * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+    * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ 
+    * created by 21343083_Mubarakh Hayatna Khairy
+*/
+package jobsheet_4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kondisi_Operator1 {
+    
+    public static void main(String[]args){
+        int score = 0;
+        char answer = 'a';
+        
+        score = (answer == 'b') ? 10 : 0;
+        System.out.println("Score = " +score);
+    }
+}
---
+++ JobSheet_4/src/jobsheet_4/Test_AND.java
@@ -0,0 +1,32 @@
+/*
+    * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+    * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ 
+    * created by 21343083_Mubarakh Hayatna Khairy
+*/
+package jobsheet_4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Test_AND {
+    
+    public static void main(String[]args){
+        int i=0;
+        int j=10;
+        boolean test = true;
+        
+        //demonstrasi &&
+        test = (i>j) && (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+        
+        //demonstrasi &
+        test = (i>j) & (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+    }  
+}
